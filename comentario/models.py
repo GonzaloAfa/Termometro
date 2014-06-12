d0a98a0616b4ee639092393359a93802ddd9ba2a
gonzaloafa <gonzaloafa@afachile.cl>
2014-06-11 22:42:28 -0400

    Se registra el comentario de la pregunta

--- comentario/models.py
+++ comentario/models.py
@@ -3,7 +3,7 @@ from registrarse.models import Usuario
 from preguntar.models import Pregunta
 
 class Comentario(models.Model):
-	usuario 	= models.ForeignKey(Usuario)
+	usuario 	= models.CharField(max_length=15)
 	texto		= models.TextField()
 	fecha		= models.DateTimeField(auto_now=True)
 	id_pregunta = models.ForeignKey(Pregunta)
